b83913d360ea361f03ed825d0ee80fe817ce9104
allen <getlose2011@gmail.com>
2016-04-20 22:57:32 +0800

    增加上下頁

--- application/language/en/news_lang.php
+++ application/language/en/news_lang.php
@@ -1,4 +1,8 @@
 <?php
 $lang['html_title'] = "bobi::news";
 $lang['title'] = "News";
+
+//footer
+$lang['page_pre'] = "prev_page";
+$lang['page_next'] = "next_page";
 ?>
--- application/language/tw/news_lang.php
+++ application/language/tw/news_lang.php
@@ -1,4 +1,9 @@
 <?php
 $lang['html_title'] = "bobi::最新消息";
 $lang['title'] = "最新消息";
+
+
+//footer
+$lang['page_pre'] = "上一頁";
+$lang['page_next'] = "下一頁";
 ?>
