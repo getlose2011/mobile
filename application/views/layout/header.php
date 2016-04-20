93c2df35fd7876725ccd7e98314db6c4cc55b4dc
allen <getlose2011@gmail.com>
2016-04-20 23:00:09 +0800

    js url and id add

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -6,9 +6,10 @@
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
 	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+	 <script type="text/javascript" src="<?php echo base_url(); ?>Resource/js/function.js"></script>
 </head>
 <body>
-	<div data-role="page">
+	<div data-role="page" id="page">
 		<div data-role="header">
 			<h1><?php echo $this->lang->line('title');?></h1>
 		</div><!-- /header -->
