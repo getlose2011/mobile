ddf21ed838f7859ee10c6a2c51a1d1531444690c
allen <getlose2011@gmail.com>
2016-04-20 22:59:41 +0800

    修改copyright

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,9 +1,34 @@
 		<div data-role="footer">
+			<div data-role="header" data-position="inline">
+				<?php
+					$go_page_pre = ($page == 1)?'':($page-1);
+					$go_page_next = ($page == $total_pages)?'':($page+1);
+				?>
+				<a href="javascript:setPageSubmit(<?php echo $go_page_pre?>);" data-icon="arrow-l" style="<?php echo empty($go_page_pre)?' display: none;':''?>"><?php echo $this->lang->line('page_pre');?></a>
+				<a href="javascript:setPageSubmit(<?php echo $go_page_next?>);" data-icon="arrow-r" style="<?php echo empty($go_page_next)?' display: none;':''?>"><?php echo $this->lang->line('page_next');?></a>
+			</div>
 			<h4>
-				BOBI好運網 版權所有 Copyright © 2010,2013. Lucky Bobi. <br />
-				All Rights Reserved. E-mail : luckybobi168@gmail.com 
+				
 			</h4>
 		</div><!-- /footer -->
 	</div><!-- /page -->
+	<script>
+	var total_pages = <?php echo $total_pages?>,
+		page = <?php echo $page?>,
+		go_page = '';
+	$(document).on('pageinit', '#page', function () {
+
+		  $(document).on("swiperight", function () {        //往右滑的動作
+		   	 	go_page = (page == 1)?total_pages:(page-1);
+		   	 	setPageSubmit(go_page);
+		  });
+
+		  $(document).on("swipeleft", function () {         //往左滑的動作
+				go_page = (page == total_pages)?1:(page+1);
+		  		setPageSubmit(go_page);
+		  });
+
+		});
+	</script>
 </body>
 </html>
