510481c0f91da5befde38c7faa25fae988e4e766
allen <getlose2011@gmail.com>
2016-04-23 11:27:23 +0800

    增加link_para function

--- Resource/js/function.js
+++ Resource/js/function.js
@@ -9,9 +9,22 @@ function QueryString(name)
     }
     return "";
 }
+
 function setPageSubmit(page){
     var formname = 'mainform';
     document.forms[formname].method = 'get';
     $('form[name="'+formname+'"]').append('<input type="hidden" name="page" id="page" value="'+page+'" />');
+    document.forms[formname].submit();
+}
+
+function link_para(id, val){
+    var formname = 'mainform';
+    document.forms[formname].method = 'get';
+    if($("#"+id).length > 0)
+    {
+        $("#"+id).val(val);
+    }else{
+        $('form[name="'+formname+'"]').append('<input type="hidden" name="'+id+'" id="'+id+'" value="'+val+'" />');
+    }
     document.forms[formname].submit();
 }
