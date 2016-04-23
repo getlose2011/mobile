896a66fa0102e0bea274afff5e1fb5e0461cf62f
allen <getlose2011@gmail.com>
2016-04-23 11:26:16 +0800

    多增語言

--- application/language/en/news_lang.php
+++ application/language/en/news_lang.php
@@ -1,8 +1,15 @@
 <?php
+//header
 $lang['html_title'] = "bobi::news";
 $lang['title'] = "News";
+$lang['left_menu'] = "Menu";
+$lang['right_menu'] = "Menu1";
+
+
+//panel
+$lang['close_panel'] = "Close Panel";
 
 //footer
-$lang['page_pre'] = "prev_page";
-$lang['page_next'] = "next_page";
+$lang['page_pre'] = "Previous";
+$lang['page_next'] = "Next";
 ?>
--- application/language/tw/news_lang.php
+++ application/language/tw/news_lang.php
@@ -1,6 +1,13 @@
 <?php
+//head
 $lang['html_title'] = "bobi::最新消息";
 $lang['title'] = "最新消息";
+$lang['left_menu'] = "目錄";
+$lang['right_menu'] = "目錄1";
+
+
+//panel
+$lang['close_panel'] = "返回";
 
 
 //footer
